de798f69030cc16fafa720735a66a7413d6458e8
BonYGames1 <104096915+BonYGames1@users.noreply.github.com>
2022-04-20 20:39:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace soubory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader cteni = new StreamReader("input.txt");
+
+
+            string radek;
+
+            while ((radek = cteni.ReadLine()) != null)
+            {
+                Console.WriteLine(radek);
+            }
+
+            cteni.Close();
+
+            StreamWriter zapis = new StreamWriter("output.txt",true);
+
+            zapis.WriteLine("amogus neventoval");
+
+            zapis.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
